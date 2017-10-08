a99b8e6b4ab0dbed0d10a68cd62d12244b85c273
Nghia Tran <vinhnghiatran@gmail.com>
2017-10-08 23:07:59 +0700

    Improved PersistentHandler

---
+++ Sources/Storage/Disk/String+Base64.swift
@@ -0,0 +1,24 @@
+//
+//  String+MD5.swift
+//  StatsdClient
+//
+//  Created by Nghia Tran on 10/8/17.
+//  Copyright © 2017 StatsdClient. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    func fromBase64() -> String? {
+        guard let data = Data(base64Encoded: self) else {
+            return nil
+        }
+
+        return String(data: data, encoding: .utf8)
+    }
+
+    func toBase64() -> String {
+        return Data(self.utf8).base64EncodedString()
+    }
+}
