f319e510f3b47f19c3ccead4796c4cb1fb6881e4
Khoi Lai <khoi.geeky@gmail.com>
2017-10-11 14:51:38 +0700

    Add support for TCP Client

--- Sources/Transport/TCP/TCPTransport.swift
+++ Sources/Transport/TCP/TCPTransport.swift
@@ -1,24 +1,26 @@
 //
-//  UDPClient.swift
+//  TCPTransport.swift
 //  StatsdClient
 //
-//  Created by Khoi Lai on 9/10/17.
+//  Created by Khoi Lai on 10/11/17.
 //  Copyright © 2017 StatsdClient. All rights reserved.
 //
+
 import Foundation
 import CocoaAsyncSocket
 
-public class UDPTransport: NSObject, Transport {
+public class TCPTransport: NSObject, Transport {
     let host: String
     let port: UInt16
 
-    private lazy var socket: GCDAsyncUdpSocket = {
-        let socket = GCDAsyncUdpSocket(delegate: self, delegateQueue:
-            DispatchQueue(label: "UDPClient_Delegate_Queue"))
-        return socket
-    }()
     private var completionBlocks = [Int: TransportCompletionCallback]()
     private var tag: Int = 0
+
+    private lazy var socket: GCDAsyncSocket = {
+        let socket = GCDAsyncSocket(delegate: self,
+                                    delegateQueue: DispatchQueue(label: "TCPClient_Delegate_Queue"))
+        return socket
+    }()
 
     public init(host: String, port: UInt16) {
         self.host = host
@@ -37,18 +39,12 @@ public class UDPTransport: NSObject, Transport {
         }
         tag += 1
 
-        completionBlocks[tag] = completion
-
-        socket.send(data,
-                    toHost: host,
-                    port: port,
-                    withTimeout: -1,
-                    tag: tag)
+        socket.write(data, withTimeout: -1, tag: tag)
     }
 }
 
-extension UDPTransport: GCDAsyncUdpSocketDelegate {
-    public func udpSocket(_ sock: GCDAsyncUdpSocket, didSendDataWithTag tag: Int) {
+extension TCPTransport: GCDAsyncSocketDelegate {
+    public func socket(_ sock: GCDAsyncSocket, didWriteDataWithTag tag: Int) {
         guard let callback = completionBlocks[tag] else {
             return
         }
@@ -56,12 +52,12 @@ extension UDPTransport: GCDAsyncUdpSocketDelegate {
         completionBlocks[tag] = nil
     }
 
-    public func udpSocket(_ sock: GCDAsyncUdpSocket, didNotSendDataWithTag tag: Int, dueToError error: Error?) {
-        guard let callback = completionBlocks[tag] else {
-            return
-        }
-
-        callback(error)
+    public func socket(_ sock: GCDAsyncSocket,
+                       shouldTimeoutWriteWithTag tag: Int,
+                       elapsed: TimeInterval,
+                       bytesDone length: UInt) -> TimeInterval {
+        completionBlocks[tag]?(TransportError.timeout)
         completionBlocks[tag] = nil
+        return -1
     }
 }
