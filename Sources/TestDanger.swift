f9fb606941009057cbae9960bbac86ad86d66511
Nghia Tran <vinhnghiatran@gmail.com>
2017-10-11 22:19:02 +0700

    Check danger bot

---
+++ Sources/TestDanger.swift
@@ -0,0 +1,23 @@
+//
+//  TestDanger.swift
+//  StatsdClient
+//
+//  Created by Nghia Tran on 10/11/17.
+//  Copyright © 2017 StatsdClient. All rights reserved.
+//
+
+import Foundation
+
+class TestDanger {
+
+    var data:[Int]=[123]
+    func doSomething() {
+
+        let foo:Any = ["foo"]
+        let _=foo as? [String]
+    }
+
+
+
+
+}
